0bdb4edbcc83e4742b25277a125bfa59afb571d1
Chamika Dilshan <chamidilshan55@gmail.com>
2024-01-20 14:28:34 +0530

    added styles for hero texts

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,14 +18,20 @@ const Hero = () => {
       justify-center items-start
       w-full max-xl:padding-x pt-28
       ">
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>Our New Arrival</span>
+        <p className="text-xl font-montserrat
+        text-coral-red">Our Summer Collection</p>
+        <h1 className="mt-10 font-palanquin
+         text-8xl max-sm:text-[72px] max-sm:leading-[82]
+         font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap
+          relative z-10 pr-10">Our New Arrival</span>
           <br/>
-          <span>Nike</span>
-          Shoes
+          <span className="text-coral-red
+          inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, Quality
+        <p className="font-montserrat
+        text-slate-gray text-lg
+        leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, Quality
           and comfort all in one place. Make your 
           life easier and more comfortable with
           our collection.
@@ -40,8 +46,10 @@ const Hero = () => {
         mt-20 justify-start gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl
+              font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7
+              font-montserrat text-slate-gray">{stat.label}</p>
               </div>
               ))} 
         </div>
